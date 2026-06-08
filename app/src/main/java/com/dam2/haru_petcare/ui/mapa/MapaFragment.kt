5ac5b14b8960071f23f2e826cf93639a57ac6740
Ana Belloso Gonzalez <anavbellosog@gmail.com>
2026-06-08 02:08:30 +0200

    Definitivo(?

--- app/src/main/java/com/dam2/haru_petcare/ui/mapa/MapaFragment.kt
+++ app/src/main/java/com/dam2/haru_petcare/ui/mapa/MapaFragment.kt
@@ -13,6 +13,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dam2.haru_petcare.R
